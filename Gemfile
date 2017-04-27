a758e615ec216a95bf05d2b4820c8830c7011453
satendra <satendra@satendra.nppgl.com>
2017-04-27 18:27:17 +0530

    Changes

--- Gemfile
+++ Gemfile
@@ -32,13 +32,19 @@ gem 'jbuilder', '~> 2.5'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
+gem "responders"
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
+
+# angular-js
+
 gem 'angularjs-rails'
 gem 'angular-ui-bootstrap-rails' #for bootstrap UI
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
+  gem 'pry'
 end
 
 group :development do
--- app/assets/javascripts/angular/controllers/users_controllers.js
+++ app/assets/javascripts/angular/controllers/users_controllers.js
@@ -1,50 +1,116 @@
-var myApp = angular.module('myapplication', ['ngRoute', 'ngResource']);
+var myApp = angular.module('myapplication', ['ngRoute', 'ngResource']); 
 
-/*Factoriesssss*/
-myApp.factory('Users', ['$resource', function($resource){
-	return $resource('/users.json', {}, {
-		query: {method: 'GET', isArray: true },
-		create: {method: 'POST'}
-	})
+//Factory
+myApp.factory('Users', ['$resource',function($resource){
+  return $resource('/users.json', {},{
+    query: { method: 'GET', isArray: true },
+    create: { method: 'POST' }
+  })
 }]);
-
 
 myApp.factory('User', ['$resource', function($resource){
-	return $resource('/users/:id.json', {}, {
-		show: {method: 'GET' },
-		update: { method: 'PUT', params:{id: '@id'}},
-		delete: { method: 'DELETE', params: {id: '@id'}}
-	});
+  console.log("asdasdadsdasdada");
+  console.log($resource);
+  return $resource('/users/:id.json', {}, {
+    show: { method: 'GET' },
+    update: { method: 'PUT', params: {id: '@id'} },
+    delete: { method: 'DELETE', params: {id: '@id'} }
+  });
+}]);
+
+//Controller
+myApp.controller("UserListCtr", ['$scope', '$http', '$resource', 'Users', 'User', '$location', function($scope, $http, $resource, Users, User, $location) {
+
+  $scope.users = Users.query();
+
+  $scope.deleteUser = function (userId) {
+    if (confirm("Are you sure you want to delete this user?")){
+      console.log("User id");
+      console.log(userId);
+      User.delete({ id: userId }, function(){
+        console.log("sdsfsdffsf");
+        $scope.users = Users.query();
+        $location.path('/');
+      });
+    }
+  };
+}]);
+
+myApp.controller("UserUpdateCtr", ['$scope', '$resource', 'User', '$location', '$routeParams', function($scope, $resource, User, $location, $routeParams) {
+  $scope.user = User.get({id: $routeParams.id})
+  $scope.update = function(){
+    if ($scope.userForm.$valid){
+      User.update({id: $scope.user.id},{user: $scope.user},function(){
+        $location.path('/');
+      }, function(error) {
+        console.log(error)
+      });
+    }
+  };
+  
+  $scope.addAddress = function(){
+    $scope.user.addresses.push({street1: '', street2: '', city: '', state: '', country: '', zipcode: '' })
+  }
+
+  $scope.removeAddress = function(index, user){
+    var address = user.addresses[index];
+    if(address.id){
+      address._destroy = true;
+    }else{
+      user.addresses.splice(index, 1);
+    }
+  };
+
+}]);
+
+myApp.controller("UserAddCtr", ['$scope', '$resource', 'Users', '$location', function($scope, $resource, Users, $location) {
+  console.log(" create new users");
+  $scope.user = {addresses: [{street1: '', street2: '', city: '', state: '', country: '', zipcode: '' }]}
+  $scope.save = function () {
+    if ($scope.userForm.$valid){
+      Users.create({user: $scope.user}, function(){
+        $location.path('/');
+      }, function(error){
+        console.log(error)
+      });
+    }
+  }
+
+  $scope.addAddress = function(){
+    $scope.user.addresses.push({street1: '', street2: '', city: '', state: '', country: '', zipcode: '' })
+  }
+
+  $scope.removeAddress = function(index, user){
+    var address = user.addresses[index];
+    if(address.id){
+      address._destroy = true;
+    }else{
+      user.addresses.splice(index, 1);
+    }
+  };
+
 }]);
 
 
-myApp.controller("UserListCtr", ['$scope', '$resource', 'Users', 'User', '$location', function($scope, $resource, Users, User, $location){
-	$scope.users = Users.query();
-}])
 
-
-
-
-
-/*Defining routes*/
-
+//Routes
 myApp.config([
-	'$routeProvider', '$locationProvider', function($routeProvider, $locationProvider){
-		$routeProvider.when('/users',{
-			templateUrl: '/templates/users/index.html',
-			controller: 'UserListCtr'
-		});
-		$routeProvider.when('/users/new', {
-			templateUrl: '/templates/users/new.html',
-			controller: 'UserAddCtr'
-		});
-		$routeProvider.when('/users/:id/edit',{
-			templateUrl: '/templates/users/edit.html',
-			controller: "UserUpdateCtr"
-		});
-		$routeProvider.otherwise({
-			redirectTo: '/users'
-		});
-	}
+  '$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+    $routeProvider.when('/users',{
+      templateUrl: '/templates/users/index.html',
+      controller: 'UserListCtr'
+    });
+    $routeProvider.when('/users/new', {
+      templateUrl: '/templates/users/new.html',
+      controller: 'UserAddCtr'
+    });
+    $routeProvider.when('/users/:id/edit', {
+      templateUrl: '/templates/users/edit.html',
+      controller: "UserUpdateCtr"
+    });
+    $routeProvider.otherwise({
+      redirectTo: '/users'
+    });
+  }
+]);
 
-	]);
